fbdc834c9b69d369e9693ba5a1c89405633fc5e4
Chris Walls <cwalls2908@gmail.com>
2018-10-11 22:44:55 -0400

    Setup boilerplate, tests and linting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-import { addProject } from './utils'
+import React, { Component } from "react"
+import "./App.css"
+import { addProject } from "./utils"
 
 const initialProps = {
   projects: [
-    addProject('IT Project', 'low')
+    addProject("IT Project", "low")
   ]
 }
 
@@ -17,9 +17,9 @@ class App extends Component {
     }
     this.renderProject = this.renderProject.bind(this)
   }
-  renderProject(project, i) {
-    // In a production instance the key should be unique
-    const key = `${project.name}-${project.key}`
+  renderProject(project, index) {
+    // In a production instance the key should be unique (primary key from db, etc)
+    const key = `${index}-${project.name}`
     return <li key={key}> {project.name} </li>
   }
   render() {
@@ -28,11 +28,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h2>Project Management</h2>
-          {projects.map((project, i) => this.renderProject(project))}
+          {projects.map((project, index) => this.renderProject(project, index))}
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/utils.test.js
+++ src/utils.test.js
@@ -1,12 +1,12 @@
-import * as utils from  './utils'
+import * as utils from  "./utils"
 
-it('validates type of date', () => {
+it("validates type of date", () => {
   const date = new Date(2018,1,1)
   expect(utils.validateDateType(date)).toBe(true)
 })
 
-it('rejects invalid dates', () => {
-  const invalidDate = ''
+it("rejects invalid dates", () => {
+  const invalidDate = ""
   expect(utils.validateDateType(invalidDate)).toBe(false)
   expect(utils.validateDateType()).toBe(false)
   expect(utils.validateDateType(null)).toBe(false)
