e0d4e6181680d86bd252411a77f36d5837262e97
Chris Walls <cwalls2908@gmail.com>
2018-10-15 12:43:05 -0400

    Adding base functionality in utils

--- src/utils.js
+++ src/utils.js
@@ -1,33 +1,148 @@
-const cost = (type) => {
-  if (type === "low") {
-    return 25
-  } else if (type === "high") {
-    return 75
-  }
-  else {
-    return false
-  }
-}
-
+// Did not write test for this
+// A test would likely be longer than the code
 const validateDateType = (date) => {
   return (Object.prototype.toString.call(date) === "[object Date]") 
     ? true 
     : false
 }
 
-exports.addProject = (name, cityType, startDate, endDate) => {
-  // Could raise error here depending on requirements
-  if (!validateDateType(startDate) || !validateDateType(endDate)) {
+const costMap = (type) => {
+  // string is probably not the best option here
+  // could be boolean or an object/model if more than 3 cost types
+  const typeIsValid = ["low","high"].indexOf(type) > -1
+  if (!typeIsValid) return
+  
+  return {
+    low:  { travel: 45, full: 75},
+    high: { travel: 55, full: 85}
+  }[type]
+}
+
+
+const sameDay = (date1, date2) => {
+  if (!validateDateType(date1)) return false
+  if (!validateDateType(date2)) return false
+  return date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate()
+}
+
+
+// returns all dates between a range
+const getDates = (startDate, endDate) => {
+  let dates = []
+  let currentDate = startDate
+  let  addDays = function(days) {
+    let date = new Date(this.valueOf())
+    date.setDate(date.getDate() + days)
+    return date
+  }
+  while (currentDate <= endDate) {
+    dates.push(currentDate)
+    currentDate = addDays.call(currentDate, 1)
+  }
+  return dates
+}
+
+// Ensures value is a number
+const isNumber = (n) => !isNaN(parseFloat(n)) && !isNaN(n - 0)
+
+// Change a date based on integer offset
+const offsetDate = (date, offset) => {
+  if (!isNumber(offset) || !validateDateType(date)) return
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate() + offset)
+}
+
+// Checks if project end points overlap or are consecutive
+const checkAdjacency = (projects, date) => {
+  let nextDay = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1)
+  let prevDay = new Date(date.getFullYear(), date.getMonth(), date.getDate() - 1)
+  return projects.some(project => {
+    let projectIndex = projects.indexOf(project)
+    let otherProjects = projects.slice(0, projectIndex).concat(projects.slice(projectIndex+1))
+    return otherProjects.some(adjacentProject => 
+      sameDay(nextDay, adjacentProject.startDate) 
+      || sameDay(prevDay, adjacentProject.endDate)
+      || sameDay(project.endDate, adjacentProject.startDate) 
+      || sameDay(project.startDate, adjacentProject.endDate) 
+    )
+  })
+}
+
+const checkTravelDay = (projects, date) =>
+  projects
+    .filter(project =>  sameDay(date, project.startDate) || sameDay(date, project.endDate))
+    .length !== 0
+
+
+// Adds project and returns false if dates are invalid (or reversed)
+exports.addProject = (name = "", cityType, startDate, endDate) => {
+  // May want to raise error or display message depending on requirements
+  if (!validateDateType(startDate)
+      || !validateDateType(endDate)
+      || startDate > endDate
+  ) {
     return false
   }
   return {
     name: name,
-    cost: cost(cityType)
+    dailyCost: costMap(cityType),
+    startDate: startDate,
+    endDate: endDate
   }
 }
 
-exports.calculateCost = (project) => {
-  return project
+
+exports.calculateProjectSetCost = (projects) => {
+  const projectSetStartDate = new Date(Math.min.apply(Math, projects.map((project)  => project.startDate)))
+  const projectSetEndDate = new Date(Math.max.apply(Math, projects.map((project)  => project.endDate)))
+  const potentialWorkDays = getDates(projectSetStartDate, projectSetEndDate)
+  const totalDays = potentialWorkDays.length
+  
+
+  // Iterate over all dates and determine full project workday or travel day
+  let projectSetArray = potentialWorkDays.map((date) => {
+    let chargeType = null
+    let isTravelDay = checkTravelDay(projects, date)
+    let isAdjactentDay = checkAdjacency(projects, date)
+    let notFirstOrLastDate = !sameDay(date, projectSetEndDate) && !sameDay(date, projectSetStartDate)
+    let isFullProjectWorkDay = projects.some((project) =>  date > project.startDate && date < project.endDate)
+
+    // Days explicitly in range of single project are always full
+    if (isFullProjectWorkDay) {
+      chargeType = "full"
+    } else {
+      if (isTravelDay) {
+        // If adjacent and not beginning or end of set it is a full day, otherwise travel
+        if (isAdjactentDay && notFirstOrLastDate) {
+          chargeType = "full"
+        } else {
+          chargeType = "travel"
+        }
+      }
+    }
+    let projectCosts = projects
+      .filter(project => date >= project.startDate && date <= project.endDate)
+      .map(project => project.dailyCost[chargeType])
+      
+    let dateCost = projectCosts.sort().reverse()[0]
+    return {
+      date,
+      dateCost,
+      chargeType,
+    }
+  })
+  
+  console.table(projectSetArray)
+  let projectSetCost = projectSetArray
+    .map(projectSetDay => projectSetDay.dateCost)
+    .filter(el => el != null)
+    .reduce((sum, current) => sum+current, 0)
+  return {potentialWorkDays, totalDays, projectSetArray, projectSetCost}
 }
 
+
 exports.validateDateType = validateDateType
+exports.isNumber = isNumber
+exports.sameDay = sameDay
+exports.offsetDate = offsetDate
--- src/utils.test.js
+++ src/utils.test.js
@@ -1,13 +1,108 @@
 import * as utils from  "./utils"
 
+// Unit Tests
+
 it("validates type of date", () => {
   const date = new Date(2018,1,1)
+  const invalidDate = "12-31-2018"
+  
   expect(utils.validateDateType(date)).toBe(true)
+  expect(utils.validateDateType(invalidDate)).toBe(false)
+  expect(utils.validateDateType(2018)).toBe(false)
+  // Should generally check null vs undefined in JS
+  expect(utils.validateDateType()).toBe(false)
+  expect(utils.validateDateType(null)).toBe(false) 
 })
 
-it("rejects invalid dates", () => {
-  const invalidDate = ""
-  expect(utils.validateDateType(invalidDate)).toBe(false)
-  expect(utils.validateDateType()).toBe(false)
-  expect(utils.validateDateType(null)).toBe(false)
+it("checks that dates are ordered correctly", () => {
+  let startDate = new Date(2018,1,1)
+  let endDate = new Date(2018,12,31)
+  
+  const validProject = utils.addProject("Example", "low", startDate, endDate)
+  const oneDayProject = utils.addProject("One Day","Low",startDate, startDate)
+  const reverseDateProject = utils.addProject("Example", "low", endDate, startDate)
+
+  expect(validProject).toBeDefined()
+  expect(oneDayProject).toBeDefined()
+  expect(reverseDateProject).toBeFalsy()
 })
+
+it("creates projects with valid input", () => {
+  let date = new Date(2018,1,1)
+  const lowProject = utils.addProject("Low Example", "low", date, date)
+  const highProject = utils.addProject("High Example", "high", date, date)
+  const expectedLowProject = {name: "Low Example", dailyCost: {travel: 45, full: 75}, startDate: date, endDate: date}
+  const expectedHighProject = {name: "High Example", dailyCost: {travel: 55, full: 85}, startDate: date, endDate: date}
+  
+  expect(lowProject).toEqual(expectedLowProject)
+  expect(highProject).toEqual(expectedHighProject)
+})
+
+it("returns false for invalid project input", () => {
+  let date = new Date(2018,1,1)
+  const invalidProject = utils.addProject("Low Example", "low", date, null)
+  
+  expect(invalidProject).toBe(false)
+})
+
+it("checks if dates are the same",() => {
+  let date = new Date(2018,1,1)
+  let dateDuplicate = new Date(2018,1,1)
+  let offsetDate = new Date(2018,1,2)
+  expect(utils.sameDay(date,dateDuplicate)).toBe(true)
+  expect(utils.sameDay(offsetDate)).toBeFalsy()
+  expect(utils.sameDay()).toBeFalsy()
+})
+
+it("checks type of number", () => {
+  let number = 3
+  let undefNumber
+  expect(utils.isNumber(3)).toBe(true)
+  expect(utils.isNumber(number)).toBe(true)
+  expect(utils.isNumber(23)).toBe(true)
+  expect(utils.isNumber(-23.32)).toBe(true)
+  expect(utils.isNumber("-23.32")).toBe(true)
+  expect(utils.isNumber("-a23.32")).toBe(false)
+  expect(utils.isNumber(undefNumber)).toBe(false)
+  expect(utils.isNumber("Test")).toBe(false)
+  expect(utils.isNumber("")).toBe(false)
+  expect(utils.isNumber()).toBe(false)
+  expect(utils.isNumber(null)).toBe(false)
+})
+
+
+it("finds an offset date",() => {
+  let date = new Date(2018,1,1)
+  let manuallyOffsetDate = new Date(2018,1,2)
+  let offsetDate = utils.offsetDate(date,1)
+  expect(utils.sameDay(manuallyOffsetDate, date)).toBe(false)
+  expect(utils.sameDay(manuallyOffsetDate, offsetDate)).toBe(true)
+  expect(utils.sameDay(date,"")).toBeFalsy()
+  expect(utils.sameDay(date,null)).toBeFalsy()
+  expect(utils.sameDay()).toBeFalsy()
+})
+
+
+
+// Integration Tests
+
+it("Adds cost correctly when adding project",() => {
+  let date = new Date(2018,1,1)
+  const lowProject = utils.addProject("Low Example", "low", date, date)
+  const highProject = utils.addProject("Low Example", "high", date, date)
+  
+  expect(lowProject.dailyCost.travel).toBe(45)
+  expect(lowProject.dailyCost.full).toBe(75)
+  expect(highProject.dailyCost.travel).toBe(55)
+  expect(highProject.dailyCost.full).toBe(85)
+})
+
+it("Saves project but returns no cost if type is not 'low' or 'high'", () => {
+  let date = new Date(2018,1,1)
+  const mediumProject = utils.addProject("Medium Example", "medium", date, date)
+  expect(mediumProject.dailyCost).toBeUndefined()
+  // check other behaviors are not affected by integration between addProject and cost
+  expect(mediumProject).toBeDefined()
+  expect(mediumProject.name).toEqual("Medium Example")
+})
+
